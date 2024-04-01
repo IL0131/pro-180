64654755c54379063db973821b0287d55b4c4beb
IL0131 <95005060+IL0131@users.noreply.github.com>
2024-03-31 20:46:22 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,56 @@
+let latitude, longitude, destination;
+
+$(document).ready(function () {
+	alert("Please allow the device to know your location!")
+	initGeolocation();
+})
+
+function initGeolocation() {
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(success);
+	}
+	else {
+		alert("Sorry, your browser does not support geolocation services.");
+	}
+}
+
+$(function () {
+	$("#navigate-button").click(function () {
+		window.location.href = `ar_weather.html?source=${latitude};${longitude}&destination=${destination[1]};${destination[0]}`
+	})
+})
+
+function success(position) {
+	longitude = position.coords.longitude;
+	latitude = position.coords.latitude
+
+	// Initializing Mapbox
+	mapboxgl.accessToken = 'pk.eyJ1IjoiYXBvb3J2ZWxvdXMiLCJhIjoiY2ttZnlyMDgzMzlwNTJ4a240cmEzcG0xNyJ9.-nSyL0Gy2nifDibXJg4fTA';
+
+	var map = new mapboxgl.Map({
+		container: 'map',
+		style: 'mapbox://styles/mapbox/streets-v11',
+		center: [longitude, latitude],
+		zoom: 4
+	});
+
+	map.addControl(
+		new MapboxGeocoder({
+			accessToken: mapboxgl.accessToken,
+			mapboxgl: mapboxgl
+		}).on('result', function (e) {
+			destination = e.result.center
+		})
+	);
+
+	var img1 = document.querySelector("#amber")
+
+	// Create a Amber Palace, Jaipur Marker and add it to the map.
+	var marker1 = new mapboxgl.Marker({
+		element: img1
+	})
+		.setLngLat([40.689403940241625, -74.0444574888065])
+		.addTo(map);
+
+	
+}
